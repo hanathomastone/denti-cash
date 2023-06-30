ae78bbadb0c5de608e1ca6dc520e4f812099c391
GoYuGyeong <yugyeong@kai-i.com>
2023-06-30 13:58:58 +0900

    UserLogin error 메세지 수정

--- src/main/java/com/kaii/dentix/domain/user/application/UserLoginService.java
+++ src/main/java/com/kaii/dentix/domain/user/application/UserLoginService.java
@@ -173,7 +173,7 @@ public class UserLoginService {
                 .orElseThrow(() -> new NotFoundDataException("회원 정보를 찾을 수 없습니다."));
 
         if (!passwordEncoder.matches(request.getUserPassword(), user.getUserPassword())){
-            throw new UnauthorizedException("아이디와 비밀번호가 일치하지 않습니다.");
+            throw new NotFoundDataException("회원 정보를 찾을 수 없습니다.");
         }
 
         String accessToken = jwtTokenUtil.createToken(user, TokenType.AccessToken);
