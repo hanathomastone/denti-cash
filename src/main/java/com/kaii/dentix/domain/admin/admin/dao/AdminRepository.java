ed6a30153ecab2fa07095a209efddc269e37fba6
GoYuGyeong <yugyeong@kai-i.com>
2023-08-01 17:39:40 +0900

    관리자 등록 API 구현 및 REST docs 작성

--- src/main/java/com/kaii/dentix/domain/admin/admin/dao/AdminRepository.java
+++ src/main/java/com/kaii/dentix/domain/admin/admin/dao/AdminRepository.java
@@ -1,7 +1,15 @@
 package com.kaii.dentix.domain.admin.admin.dao;
 
 import com.kaii.dentix.domain.admin.admin.domain.Admin;
+import com.kaii.dentix.domain.type.YnType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Optional<Admin> findByAdminLoginIdentifier(String adminIdentifier);
+
+    Optional<Admin> findByAdminNameAndAdminPhoneNumberAndAdminIsSuper(String adminName, String adminPhoneNumber, YnType adminIsSuper);
+
 }
---
+++ src/test/java/com/kaii/dentix/domain/admin/AdminControllerTest.java
@@ -0,0 +1,109 @@
+package com.kaii.dentix.domain.admin;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kaii.dentix.common.ControllerTest;
+import com.kaii.dentix.domain.admin.admin.application.AdminService;
+import com.kaii.dentix.domain.admin.admin.controller.AdminController;
+import com.kaii.dentix.domain.admin.admin.dto.AdminSignUpDto;
+import com.kaii.dentix.domain.admin.admin.dto.request.AdminSignUpRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static com.kaii.dentix.common.ApiDocumentUtils.getDocumentRequest;
+import static com.kaii.dentix.common.ApiDocumentUtils.getDocumentResponse;
+import static com.kaii.dentix.common.DocumentOptionalGenerator.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AdminController.class)
+public class AdminControllerTest extends ControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private AdminService adminService;
+
+    public AdminSignUpDto adminSignUpDto(){
+        return AdminSignUpDto.builder()
+                .adminId(1L)
+                .adminName("홍길동")
+                .adminPassword("2023")
+                .build();
+    }
+
+    /**
+     *  관리자 등록
+     */
+    @Test
+    public void adminSignUp() throws Exception{
+
+        // given
+        given(adminService.adminSignUp(any(AdminSignUpRequest.class))).willReturn(adminSignUpDto());
+
+        AdminSignUpRequest adminSignUpRequest = AdminSignUpRequest.builder()
+                .adminName("홍길동")
+                .adminLoginIdentifier("adminhong")
+                .adminPhoneNumber("01012345678")
+                .build();
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                RestDocumentationRequestBuilders.post("/admin/signUp")
+                        .content(objectMapper.writeValueAsString(adminSignUpRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("rt").value(200))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(document("admin/signUp",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("adminName").type(JsonFieldType.STRING).description("관리자 이름"),
+                                fieldWithPath("adminLoginIdentifier").type(JsonFieldType.STRING).description("관리자 아이디"),
+                                fieldWithPath("adminPhoneNumber").type(JsonFieldType.STRING).attributes(userNumberFormat()).description("관리자 연락처")
+                        ),
+                        responseFields(
+                                fieldWithPath("rt").type(JsonFieldType.NUMBER).description("결과 코드"),
+                                fieldWithPath("rtMsg").type(JsonFieldType.STRING).description("결과 메세지"),
+                                fieldWithPath("response").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                fieldWithPath("response.adminId").type(JsonFieldType.NUMBER).description("관리자 고유 번호"),
+                                fieldWithPath("response.adminName").type(JsonFieldType.STRING).description("관리자 이름"),
+                                fieldWithPath("response.adminPassword").type(JsonFieldType.STRING).description("관리자 초기 비밀번호")
+                        )
+                ));
+
+        verify(adminService).adminSignUp(any(AdminSignUpRequest.class));
+
+    }
+
+}
