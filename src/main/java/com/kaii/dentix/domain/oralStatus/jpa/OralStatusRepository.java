5914d5b3d26837930bd214b5d502fec398522f30
이호준 <hjlee@kai-i.com>
2023-09-08 10:36:33 +0900

    OralStatusRepository ID 타입 오류 수정

--- src/main/java/com/kaii/dentix/domain/oralStatus/jpa/OralStatusRepository.java
+++ src/main/java/com/kaii/dentix/domain/oralStatus/jpa/OralStatusRepository.java
@@ -3,5 +3,5 @@ package com.kaii.dentix.domain.oralStatus.jpa;
 import com.kaii.dentix.domain.oralStatus.domain.OralStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OralStatusRepository extends JpaRepository<OralStatus, Long> {
+public interface OralStatusRepository extends JpaRepository<OralStatus, String> {
 }
