a39437543d145f7c369a68dcfa8eb2856adf91f8
이호준 <hjlee@kai-i.com>
2023-09-18 15:29:20 +0900

    관리자 로그인 시 형식 validation 삭제

--- src/main/java/com/kaii/dentix/domain/admin/dto/request/AdminLoginRequest.java
+++ src/main/java/com/kaii/dentix/domain/admin/dto/request/AdminLoginRequest.java
@@ -15,13 +15,9 @@ import lombok.NoArgsConstructor;
 public class AdminLoginRequest {
 
     @NotBlank(message = "아이디는 필수입니다.")
-    @Size(min = 4, max = 12, message = "아이디는 최소 4자부터 최대 12자입니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 숫자나 영문만 사용 가능해요.")
     private String adminLoginIdentifier;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
-    @Size(min = 8, max = 20, message = "비밀번호는 최소 8자부터 최대 20자입니다.")
     @JsonSerialize(using = PasswordSerializer.class)
     private String adminPassword;
-
 }
