242dec07cc72d5f259eea2fb64c1b7bc0b677c66
GoYuGyeong <yugyeong@kai-i.com>
2023-09-20 18:09:38 +0900

    문진표 제출 API AI Model 연동

--- src/main/java/com/kaii/dentix/global/common/util/AiModelService.java
+++ src/main/java/com/kaii/dentix/global/common/util/AiModelService.java
@@ -1,6 +1,7 @@
 package com.kaii.dentix.global.common.util;
 
 import com.kaii.dentix.domain.oralCheck.dto.resoponse.OralCheckAnalysisResponse;
+import com.kaii.dentix.domain.questionnaire.dto.response.QuestionnaireAnalysisResponse;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -15,11 +16,16 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Map;
+
 @Service
 public class AiModelService {
 
-    @Value("${aiModel.apiUrl}")
-    private String aiModelApiUrl;
+    @Value("${aiModel.apiUrl.oralCheck}")
+    private String oralCheckAiModelApiUrl;
+
+    @Value("${aiModel.apiUrl.questionnaire}")
+    private String questionnaireAiModelApiUrl;
 
     private final RestTemplate restTemplate;
 
@@ -27,6 +33,9 @@ public class AiModelService {
         this.restTemplate = restTemplateBuilder.build();
     }
 
+    /**
+     *  구강검진 사진 촬영 AI Model
+     */
     @SneakyThrows
     @Async
     public OralCheckAnalysisResponse getPyDentalAiModel(MultipartFile picture) {
@@ -46,7 +55,24 @@ public class AiModelService {
 
         HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(params, headers);
 
-        return restTemplate.postForObject(aiModelApiUrl, entity, OralCheckAnalysisResponse.class);
+        return restTemplate.postForObject(oralCheckAiModelApiUrl, entity, OralCheckAnalysisResponse.class);
+    }
+
+    /**
+     *  문진표 AI Model
+     */
+    @SneakyThrows
+    @Async
+    public QuestionnaireAnalysisResponse getQuestionnaireAiModel(Map<String, Map<String, Object>> survey) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("survey", survey);
+
+        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(params, headers);
+
+        return restTemplate.postForObject(questionnaireAiModelApiUrl, entity, QuestionnaireAnalysisResponse.class);
     }
 
 }
