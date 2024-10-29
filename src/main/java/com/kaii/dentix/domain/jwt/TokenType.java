a539598ff6873af2f345ce0e3d597763a829cc64
고유경 <yugyeong@kai-i.com>
2024-10-29 13:25:23 +0900

    AccessToken 유효기간 1년 (임시)

--- src/main/java/com/kaii/dentix/domain/jwt/TokenType.java
+++ src/main/java/com/kaii/dentix/domain/jwt/TokenType.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum TokenType {
 
-    AccessToken(1, "토큰 유형", "Access Token", 2 * 60 * 60 * 1000L), // 2시간
+    AccessToken(1, "토큰 유형", "Access Token", 365 * 24 * 60 * 60 * 1000L), // 1년
+//    AccessToken(1, "토큰 유형", "Access Token", 2 * 60 * 60 * 1000L), // 2시간
     RefreshToken(2, "토큰 유형", "Refresh Token", 2 * 7 * 24 * 60 * 60 * 1000L); // 2주
 
     private final int id;
